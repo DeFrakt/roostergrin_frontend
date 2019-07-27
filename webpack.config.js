5c91e6c06d0b3ab95aa87908c09c965b599f8424
DeFrakt <syaussi@gmail.com>
2019-07-26 23:44:10 -0700

    RoughSetup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './src/app.js',
+
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	watch: true,
+	devServer: {
+		inline: true
+	},
+	//Add sass-loader
+	module: {
+		rules: [
+	  {
+		test: /\.(png|jpg)$/,
+  		loader: 'url-loader'
+	  },
+      {
+        test: /\.html$/,
+        loader: "html-loader"
+      },
+			{
+				test: /\.(scss)$/,
+				use: [{
+					loader: 'style-loader'
+				}, {
+					loader: 'css-loader'
+				}, {
+					loader: 'sass-loader'
+				}, {
+					loader: 'postcss-loader',
+					options: {
+						plugins: function () {
+							return [ require('autoprefixer')]
+						}
+					}
+				}]
+			}
+		]
+	}
+}
